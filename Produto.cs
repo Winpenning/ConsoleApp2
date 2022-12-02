75247aa5a15a8a83289e33db49c1a574ef15d217
zsphS <zsphS@DESKTOP-ICAV93S>
2022-12-02 19:42:37 -0300

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+namespace Course
+{
+    public class Produto
+    {
+        private string _nome; // MANIPULADO MANUALMENTE
+        private float Valor { get; set; } // MANIPULADO AUTOMATICAMENTE
+        private int Quantidade { get; set; }
+
+        public Produto() //CONSTRUTOR PADRÃO
+        {
+        }
+        public Produto(string nome, float preco, int quantidade) // SOBRCARGA
+        {
+            _nome = nome;
+            Valor = preco;
+            Quantidade = quantidade;
+        }
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+
+        public double Valortotal()
+        {
+            return Valor * Quantidade;
+        }
+
+        public void Alterar(int quantidade)
+        {
+            if (quantidade > 0)
+            {
+                Quantidade += quantidade;
+            }
+            else
+            {
+                Quantidade -= quantidade;
+            }
+        }
+        public override string ToString()
+        {
+            return "\nNome: " + _nome +
+                   "\nValor: R$" + Valor +
+                   "\nQuantidade: " + Quantidade +
+                   "\nValor em estoque: R$" + Valortotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+
+namespace Course
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite o nome do produto: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o preço do produto: ");
+            float valor = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite a quantidade do produto: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, valor, quantidade);
+            Console.WriteLine(p);
+        }
+    }
+}
